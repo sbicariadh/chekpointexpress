b6e9042d71bd3b07944915b8a2dee36110aef241
sbicariadh <sbicataha@gmail.com>
2022-12-07 07:24:58 +0100

    2 version

--- Middelweare/Date.js
+++ Middelweare/Date.js
@@ -1,12 +1,17 @@
-
 const date=(req,res,next)=>{
     let date = new Date();
-    let h =date.getHours()
-    
-    if( !(h >= 9 &&  h <= 11)    ){
-    res.send('you are not welcome ')
+    let h = date.getHours();
+    let d =date.getDay();
+
+
+    if( !(h <= 9 && h >= 17) || ( d == 0 || d == 6 ) )  {
+        res.send('close') 
+    }
+    else{
+        res.sendFile(__dirname +'/home.html' )
     }
     next()
-    }
-    
-    module.exports=date
+
+}
+
+module.exports=date
--- server.js
+++ server.js
@@ -1,27 +1,28 @@
 const express = require('express')
-const date = require ('./midelware/Date')
+const date = require('./Middelweare/date')
 const app = express()
 
-const port = 5001
 
-//route 
+const port = 5000
+
 app.get('/get',(req,res)=>{
-    res.send('you did first express project ')
+    res.send('you did well,, sa77it')
 })
 
-// jiben les fichiers html 
-app.use('/static',express.static(__dirname +'/public'))
+app.use('/static',express.static(__dirname + '/public'))
 
 
-app.get('/homePage',date,(req,res)=>{
+app.get('/homepage',date,(req,res)=>{
     res.sendFile(__dirname + '/public/home.html')
 })
 
-app.get('/productPage',(req,res)=>{
+app.get('/contactpage',(req,res)=>{
     res.sendFile(__dirname + '/public/produit.html')
 })
 
 
+
+
 app.listen(port,err=>{
-err? console.log(err):console.log(`you are connected to the ${port}`)
+    err? console.log(err):console.log(` connecté bravo, fel port  ${port}`)
 })
